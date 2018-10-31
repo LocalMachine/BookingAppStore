4a7ca8e16727f2091f3dd5073961bb208598033f
Regonda Hosta <regonda555@mail.ru>
2018-10-31 12:30:23 +0300

    Создал 2 модели: 1) Book 2) Purchase Создал контекст данных: BookContext Создал инициализатор для добавления данных в бд Добавили в WebConfig  строку подключени к бд

---
+++ BookingAppStore/Models/BookContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookingAppStore.Models
+{
+    public class BookContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; } // Если модель называется Book То свойство должно назваться во множественном числе
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookDbInitializer: DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Hi Bro", Author = "Arman Z", Pice = 100 });
+            db.Books.Add(new Book { Name = "Viski hvatit", Author = "Garik E", Pice = 200 });
+            db.Books.Add(new Book { Name = "Brat", Author = "Rach M", Pice = 300 });
+
+            base.Seed(db);
+        }
+    }
+}
