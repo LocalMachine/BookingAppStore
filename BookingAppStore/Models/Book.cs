cab0fafaeaee4ed56d1816aeeb522d008d910d56
Regonda Hosta <regonda555@mail.ru>
2018-10-31 16:45:03 +0300

    Добавил в HomeController  данные книг из бд и присвоил их к ViewBag Изменил файл  Index.cshtml. Вывел данные на этой же страницы с помощью html  и языка  razor (C#) для перебора данных из бд

--- BookingAppStore/Models/Book.cs
+++ BookingAppStore/Models/Book.cs
@@ -10,6 +10,6 @@ namespace BookingAppStore.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
-        public int Pice { get; set; }
+        public int Price { get; set; }
     }
 }
--- BookingAppStore/Models/BookContext.cs
+++ BookingAppStore/Models/BookContext.cs
@@ -16,9 +16,9 @@ namespace BookingAppStore.Models
     {
         protected override void Seed(BookContext db)
         {
-            db.Books.Add(new Book { Name = "Hi Bro", Author = "Arman Z", Pice = 100 });
-            db.Books.Add(new Book { Name = "Viski hvatit", Author = "Garik E", Pice = 200 });
-            db.Books.Add(new Book { Name = "Brat", Author = "Rach M", Pice = 300 });
+            db.Books.Add(new Book { Name = "Hi Bro", Author = "Arman Z", Price = 100 });
+            db.Books.Add(new Book { Name = "Viski hvatit", Author = "Garik E", Price = 200 });
+            db.Books.Add(new Book { Name = "Brat", Author = "Rach M", Price = 300 });
 
             base.Seed(db);
         }
