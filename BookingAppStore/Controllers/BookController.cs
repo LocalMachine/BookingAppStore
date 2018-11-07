bc6b3dba4158ca06b45681871e22c6db97dac049
Regonda Hosta <regonda555@mail.ru>
2018-11-07 17:12:11 +0300

    Get  и Post  запросы, различные способы работы с параметрами

--- BookingAppStore/Controllers/BookController.cs
+++ BookingAppStore/Controllers/BookController.cs
@@ -13,5 +13,28 @@ namespace BookingAppStore.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult GetBook()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public string GetBook(string title, string author) // либо можно будет написать как метод Square, только в форме контроллера надо будет обьвить  action="GetBook" 
+        {
+            return title + " " + author;
+        }
+
+
+        public string Square() // способ передачи не обязательных параметров
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int b = Int32.Parse(Request.Params["b"]);
+            double s = a * b / 2;
+
+            return "<h2>Result = " + s + "</h2>";
+        }
     }
 }
